328da2f33d6fce78e4b5013fedf6ba66504d8a3d
Lucy Weinman <lucyweinman@Lucys-MacBook-Pro.local>
2019-02-10 16:53:43 -0500

    added rpois

--- script/make_lucy_dat.R
+++ script/make_lucy_dat.R
@@ -1,4 +1,6 @@
+install.packages("furrr")
 library(tidyverse)
+
 library(furrr)
 library(parallel)
 library(vegan)
@@ -23,19 +25,48 @@ lucydat<-future_map_dfr(1:nrow(fbees), function(x){
     thisspecies<-fbees[[x, "bee"]]
     out<-fbees %>% 
         filter(site==thissite & sampling_round== thisround & bee == thisspecies) %>% 
-        sample_n(10, replace=T)  %>%
+        sample_n(rpois(1,2), replace=T)  %>%
         group_by(site, sampling_round, bee, sociality, plant_genus, plant_species) %>%
         summarize(pollen_units=n(), uID=fbees[[x,"uniqueID"]])
     return(out)
 } )
 
-head(lucydat)
-r<-lucydat%>%
-    group_by(uID)%>%
-    summarise(richness=n(), shannon=exp(diversity(pollen_units, index="shannon")))
-hist(r$richness)
+lucydat<-lucydat%>%
+  unite("plant",c("plant_genus", "plant_species"))
+write.csv(lucydat, "lucydat1_pois.csv")
+lucydat1<-read_csv("lucydat1.csv")
+head(lucydat1)
+#shannon and richness of individual loads at each site and round
+h<-c(2,4,6)
+h/sum(h)
+simpson<-function(h){sum((h/sum(h))^2)}
+simpson(h)
+diversities<-lucydat1%>%
+    group_by(uID, site, sampling_round)%>%
+    summarise(richness=n(), shannon=exp(diversity(pollen_units, index="shannon")), 
+              simpson= 1/simpson(pollen_units))
+
+
+sample(filter(lucydat1, site=="Baldpate" &round==$uID,1, 50,
+
+
+
+unique(r$site)
+
+hist(filter(r, site=="Baldpate")$richness)
+hist(filter(r, site=="Baldpate")$shannon)
+hist(filter(r, site=="Baldpate")$simpson)
+plot(filter(r, site=="Baldpate")$richness,filter(r, site=="Baldpate")$shannon)
+plot(filter(r, site=="Baldpate")$richness,filter(r, site=="Baldpate")$simpson)
+
+
+
+ggplot
+ggplot(filter(lucydat, site=="Baldpate"), aes=(x==bee, y==) )
 hist(r$shannon)
+
 lucydat<-lucydat%>%
     unite("plant",c("plant_genus", "plant_species"))
-length(unique(lucydat$plant))
+
+
 
