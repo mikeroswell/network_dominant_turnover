b4f80cf30e1c30a6a0f3fa059da24f8f32e7ffa3
Lucy Weinman <lucyweinman@nbp-25-181-231.nbp.ruw.rutgers.edu>
2019-02-15 15:51:39 -0500

    changed method for individual accumulation so that spits out a tidy data frame started to try to do 80% threshold, functions for sampling not working imported Colleen's pollen data, joined with bee ID info, counted up # scans in which each plant taxa found for each individual. tidy and wide formats.

--- script/make_lucy_dat.R
+++ script/make_lucy_dat.R
@@ -5,6 +5,21 @@ library(furrr)
 library(parallel)
 library(vegan)
 library(bipartite)
+#colleen's data
+pollen<-read.csv("PollenData_formatted_4feb.csv")
+colleen<-read.csv("forest_pollen_spec.csv")
+pollens<-right_join(select(colleen, uniqueID,genus, species, site, date), select(pollen, -beeID))
+pollens<-unite(pollens, bee, genus, species)
+pollens<-pollens %>%
+  mutate(pollen=replace(pollen, pollen=="na", "")) %>%
+  as.data.frame()
+pollens<-pollens%>%
+  group_by(site, date, bee, uniqueID, pollen)%>%
+  summarize(counts=n())
+
+#individual data in wide format
+pollens_wide<-spread(pollens, pollen, counts)
+
 
 #michael's data
 mrdat<-read.csv("data/2016_male_bee_dataset.csv")
@@ -58,6 +73,7 @@ lucydat<-lucydat%>%
 write.csv(lucydat, "lucydat1_pois.csv")
 
 lucydat1<-read_csv("lucydat1.csv")
+colleen<-read.csv("")
 head(lucydat1)
 lucydat1<-lucydat1%>%
   unite("plant",c("plant_genus", "plant_species"))
@@ -76,8 +92,8 @@ diversities<-lucydat1%>%
 n<-sample(filter(lucydat1, site=="Baldpate" &sampling_round== "1")$uID,50, replace=T)
 
 #looking at individual richness, simpson and shannon of lucydat1 at Baldpate
-hist(filter(r, site=="Baldpate")$richness)
-hist(filter(r, site=="Baldpate")$shannon)
+hist(filter(diversities, site=="Baldpate")$richness)
+hist(filter(diversities, site=="Baldpate")$shannon)
 hist(filter(r, site=="Baldpate")$simpson)
 plot(filter(r, site=="Baldpate")$richness,filter(r, site=="Baldpate")$shannon)
 plot(filter(r, site=="Baldpate")$richness,filter(r, site=="Baldpate")$simpson)
@@ -94,65 +110,48 @@ Bald1
 Bald1
 ggplot(Bald1, aes(x=bee, y=richness) )+
   geom_jitter()
+
 #######FOR EACH BEE SPECIES, ACCUMULATION OF POLLEN TAXA ACROSS INDIVIDUAL LOADS.
 #each individual collected ten grains of pollen. 
 
 #pollen accumulation data (across loads) for all species at Baldpate
-
+Baldpate2<-filter(lucydat1, site=="Baldpate"&sampling_round=="2")
 species<-unique(Baldpate2$bee)
-rich<-list()
 
-eachspecies<-list()
-for (k in 3:3){
+blah<-c("richness","iteration","individuals","species")
+for (k in 1:length(species)){
   sp<-filter(Baldpate2, bee==species[k])
   IDs<-unique(sp$uID)
-  it<-list()
 for (j in 1:length(IDs)){
-  rich<-list()
 for(i in 1:50){
-sampleIDs<-sp[sample(length(IDs), j, replace=T),]
+sampleIDs<-sp[sample(length(IDs), j, replace=F),]
 sample<-filter(sp, uID%in%sampleIDs$uID)
-rich[i]<-length(unique(sample$plant))
-it[[j]]<-rich
 
-}
-  ##this is my attempt to "unlist" within the loop, so that "eachspecies"
-  #would be a list of dataframes, one for each species. Does not work.
-  #accumulation<-data.frame(matrix(unlist(it), nrow=50, byrow=T))
-  #n<-1:length(IDs)
-  #p<-rep("'", length(IDs))
-  #names(accumulation)<-cbind(p,n)[,2]
-  #acc<-gather(accumulation, individuals, names(accumulation))
-  #names(acc)<-c("individuals", "richness")
-  #acc$individuals<-as.numeric(acc$individuals)
-  #acc<-arrange(acc, individuals)
-  #eachspecies[[k]]<-acc 
-  
-  eachspecies[[k]]<-it
-  
-}
-}
-##^this works, but don't know how to deal with the stuff in eachspecies to make curves. 
+blah<-rbind(blah,c(length(unique(sample$plant)),i,j,k)) 
 
-#stuff
-accumulation<-data.frame(matrix(unlist(eachspecies[[3]]), nrow=50, byrow=T))
+}}
+}
 
-ggplot(acc, aes(x=as.factor(individuals), y=richness))+
-  geom_boxplot()
-eachspecies[[1]]
-acc<-acc%>%
-  group_by(individuals)%>%
-  summarize(meanrich=mean(richness))
-ggplot(acc, aes(x=as.factor(individuals), y=meanrich))+
-  geom_point()
+blah<-as.data.frame(blah)
+names(blah)<-c("richness","iteration","individuals","species")
+blah<-blah[2:length(blah$richness),]
+
+onebee<-filter(blah, species=="5")
+onebee$individuals<-as.numeric(onebee$individuals)
+onebee<-arrange(onebee, individuals)
+
+ggplot(onebee, aes(x=individuals, y=as.numeric(richness)))+
+  geom_boxplot(aes(group=individuals))
+
+
 #############BEE-SPECIES LEVEL pollen stuff
 #bee species level pollen composition; the number of "grains" of each plant collected by each bee species 
 
-lucydat1<-lucydat1%>%
+speciespollen<-lucydat1%>%
   group_by(site, sampling_round, bee, plant)%>%
   mutate(grains=sum(pollen_units))
 
-pollencomp<-select(lucydat1, c("site", "sampling_round", "bee", "plant", "grains"))
+pollencomp<-select(speciespollen, c("site", "sampling_round", "bee", "plant", "grains"))
 pollencomp<-unique(pollencomp)
 pollencomp<-arrange(pollencomp, bee)
 
@@ -171,9 +170,55 @@ B2comp_wide<-B2comp_wide%>%
 B2comp_wide<-B2comp_wide%>%
   select(bee, everything())
 
-rowSums(B2comp_wide, na.rm=T)
 
 
+#select pollens that meet 80% threshold for each species. subset full species by pollen taxa data frame
+#for each bee's vector of pollen abundances, randomly select a pollen. if it's grains
+#meet or exceed 0.8*sum(vector), return a 1 for that species and don't add any other species. if it's 
+#grains don't meet or exceed that, return a 1 and then add another species. if the total # grains between the
+#two species meets or exceeds the threshold, then done. if not, continue adding species etc. 
 
+###OR randomly sample 80% of each species pollen in each iteration. the pollen taxa that 
+#are in the 80% in that iteration are the dominant species for that bee species in that iteration.
+#function to take an abundance vector and subsample to size
 
+#Michael's functions to do the second thing:
+subsam<-function(ab_vec, size=0.8*sum(ab_vec)){
+  ab<-as.numeric(ab_vec)
+  spp<-length(ab)
+  l<-1:spp
+  inds<-unlist(lapply(l, function(x){
+    rep(x, ab[x])
+  }))
+  sam<-sample(inds, size=size, replace=F)
+  ss<-unlist(lapply(1:spp, function(y){
+    length(which(sam==y))
+  }))
+  return(ss)
+}
+?apply
+#take a subset of a bunch of community vectors, each subset of equal size 
+subcom<-function(com){
+  t(apply(com, 1,subsam #function(y){
+    #subsam(y)}
+  ))
+}
 
+#then need to create accumulation of the dominant pollens identitified in the last step across species
+#convert to tidy format and use approach from individual accumulation?
+noID<-as.data.frame(B2comp_wide[,2:23])
+subsam(noID[1,])
+t<-length(noID[1,])
+t
+1:t
+as.vector(noID[1,1:t])
+lapply(1:t, function(x){
+  rep(x, noID[1,x])
+})
+rep(1,B2comp_wide[1,1])
+B2comp_wide[1,1]
+domcomp<-subcom(noID)
+
+first<-noID[1,]
+as.numeric(first[1])
+first[1]
